65b324a863690e01452e4e029a887ab4016784fb
Edgar Benzor <edgarbenz@hotmail.com>
2022-05-24 12:56:10 -0500

    Preparar el proyecto y uso de children

---
+++ src/componentes/Boton.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Boton(props) {
+  const esOperador = valor => {
+    return isNaN(valor) && (valor !== '.') && (valor !== '=')
+  }
+  return (
+    <div
+      className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`}>
+      {props.children}
+    </div>
+  )    
+}
+export default Boton;
